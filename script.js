7f75260104968a653756d1188862c1900ddb9c31
Arpit <111arpit1@gmail.com>
2022-02-01 00:28:56 +0530

    update

--- script.js
+++ script.js
@@ -47,9 +47,10 @@ fetch('style_links.json')
 
             <div class="btn-group">
             <a target="_blank" href="${links[i].link}">View</a>
-            <a target="_blank" href="https://github.com/arpit456jain/Amazing-Css-Effects/tree/master/${links[i].name}" class="code">Code</a>
+            
             </div>
             </div>`;
             document.getElementById('styles').appendChild(StyleBoxDiv);
         };
 });
+    // <a target="_blank" href="https://github.com/arpit456jain/Amazing-Css-Effects/tree/master/${links[i].name}" class="code">Code</a>
