29094765361e683df8bf28162984c4688fcf84a6
Tarashdeep Singh <92468703+starashdeep@users.noreply.github.com>
2022-03-04 20:18:51 +0530

    Add files via upload

---
+++ AT-AT_walker/script.js
@@ -0,0 +1,7 @@
+/*
+  STAR WARS
+  AT-AT WALKER (ALL TERRAIN ARMORED TRANSPORT)
+  
+  I really liked that little AT-AT Walker           animation so I decided to recreate it in good     pure CSS only. No JS was harmed. 
+  Just for fun!
+*/
