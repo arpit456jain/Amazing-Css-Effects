1c23ab928243b3aea60a2e12375468832b910fb0
Abhishek SIngh <71981463+Abh1shekSingh@users.noreply.github.com>
2022-04-01 12:33:41 +0530

    New Fresh UI

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 // Navbar Toggling starts
 
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+//NavBar
+$(document).ready(function () {
+    $("ul.navbar-nav a").click(function (e) {
+     $("ul.navbar-nav a").removeClass("active");
+     $(this).addClass("active");
+      });
+  });
 
 // Navbar Toggling end 
 
@@ -30,27 +29,76 @@ scrolltop.on('click', function(e) {
 
 // scroll to top ends
 
+let projects = [] ;
+const experi = document.getElementById("cardd")
+const ProjectCounter = document.getElementById('counter');
+const searchBar = document.getElementById("searchBar");
 
-fetch('style_links.json')
-    .then(response => response.json())
-    .then(data => {
-        let links = data;
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value;
+    const filteredProjects = projects.filter( projects => {
+        
+        return projects.name.toLowerCase().includes(searchString.toLowerCase());
+    });
 
-        for (let i = 0; i < links.length; i++) {
-            console.log(links.length);
-            let StyleBoxDiv = document.createElement('div');
-            StyleBoxDiv.innerHTML = 
-            `<div class="stylebox">
-                <div class="image">
-                    <img src="https://raw.githubusercontent.com/arpit456jain/Amazing-Css-Effects/master/${links[i].name}/preview.png" alt="">
-                </div>
-                <div class="card-data">
-                    <p class="card-heading">${links[i].name}</p>
-                    <a href=${links[i].link}><button class="btnn">View</button></a>
-                </div>
-            
-            </div>`;
-            document.getElementById('styles').appendChild(StyleBoxDiv);
-        };
+    displayProjects(filteredProjects);
 });
+
+const getProject = fetch('style_links.json')
+                .then(response => response.json())
+                .then(data => {
+                    projects = data;
+                    displayProjects(projects)
+
+});
+
+ // <a target="_blank" href="https://github.com/arpit456jain/Amazing-Css-Effects/tree/master/${links[i].name}" class="code">Code</a>
+ const displayProjects = (projects) => {
+
+      
+    const htmlString = projects
+    .map((project) => {
+      console.log(projects.length)
+        return`<div class="stylebox">
+          
+              <img class="image" src="/${project.name}/preview.png" alt="" >
+              <div class="card-data">
+                  <p class="card-heading">${project.name}</p>
+                  <a href=${project.link}><button class="btnn">View</button></a>
+              </div>
+       
+          </div>`;
+          
+    })
+   
+    // allProjectList.innerHTML = htmlString;
+    experi.innerHTML = htmlString
+    ProjectCounter.innerHTML = projects.length;
+    
+}
+
+getProject();
+
+// fetch('style_links.json')
+//     .then(response => response.json())
+//     .then(data => {
+//         let links = data;
+
+//         for (let i = 0; i < links.length; i++) {
+//             console.log(links.length);
+//             let StyleBoxDiv = document.createElement('div');
+//             StyleBoxDiv.innerHTML = 
+//             `<div class="stylebox">
+//                 <div class="image">
+//                     <img src="https://raw.githubusercontent.com/arpit456jain/Amazing-Css-Effects/master/${links[i].name}/preview.png" alt="">
+//                 </div>
+//                 <div class="card-data">
+//                     <p class="card-heading">${links[i].name}</p>
+//                     <a href=${links[i].link}><button class="btnn">View</button></a>
+//                 </div>
+            
+//             </div>`;
+//             document.getElementById('styles').appendChild(StyleBoxDiv);
+//         };
+// });
     // <a target="_blank" href="https://github.com/arpit456jain/Amazing-Css-Effects/tree/master/${links[i].name}" class="code">Code</a>
